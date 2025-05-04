7f481d88a999d08fbf09b654d38747b163e6a74f
Jordan Andersen <bokeh.yap.0l@icloud.com>
2025-05-04 17:16:30 -0400

    Minor corrections.

--- src/model/ColumnData.java
+++ src/model/ColumnData.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 /**
  * Represents metadata for a database table column.
  * Contains the column name, its SQL data type, and a flag indicating
@@ -7,43 +9,53 @@ package model;
  *
  * @author -- Brad Jn-Baptiste
  */
-public class ColumnData{
+public class ColumnData {
         /// The name of the column.
-        String name;
+        private final String name;
 
         /// The SQL type name of the column (e.g., VARCHAR, INTEGER).
-        String type;
+        private final String type;
 
         /// True if this column is part of the primary key; false otherwise.
-        boolean isPrimaryKey;
+        private final boolean isPrimaryKey;
 
-public ColumnData(String name, String type, boolean isPrimaryKey){
-        this.name = name;
-        this.type = type;
-        this.isPrimaryKey = isPrimaryKey;
-}
+        public ColumnData(String name, String type, boolean isPrimaryKey) {
+                this.name = name;
+                this.type = type;
+                this.isPrimaryKey = isPrimaryKey;
+        }
 
-public String getName(){
-        return name;
-}
-public void setName(String name){
-        this.name = name;
-}
-public String getType(){
-        return type;
-}
-public void setType(){
-        this.type = type;
-}
-public boolean isPrimaryKey(){
-        return isPrimaryKey;
-}
-public void setPrimaryKey(boolean isPrimaryKey){
-        isPrimaryKey = isPrimaryKey;
-}
+        public String getName() {
+                return name;
+        }
 
-@Override
-public String toString(){
-        return name + " (" + type + ") " + (isPrimaryKey ? " Primary Key: " : "");
+        public String getType() {
+                return type;
+        }
+
+        public boolean isPrimaryKey() {
+                return isPrimaryKey;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                ColumnData that = (ColumnData) o;
+                return isPrimaryKey() == that.isPrimaryKey() && Objects.equals(getName(), that.getName()) && Objects.equals(getType(), that.getType());
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(getName(), getType(), isPrimaryKey());
+        }
+
+        @Override
+        public String toString() {
+                return "ColumnData{" +
+                        "name='" + name + '\'' +
+                        ", type='" + type + '\'' +
+                        ", isPrimaryKey=" + isPrimaryKey +
+                        '}';
         }
 }
