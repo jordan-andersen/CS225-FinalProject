0eaf2d153a696d31701e80373b196f6c43577215
Jordan Andersen <bokeh.yap.0l@icloud.com>
2025-05-06 00:14:53 -0400

    Minor corrections.

--- src/model/User.java
+++ src/model/User.java
@@ -1,17 +1,17 @@
 package model;
 
-
-
-// coded by Valerie Holland
+import java.util.Objects;
 
 /**
  * Represents an application user with a name and assigned role.
+ * @author Valerie Holland
  */
-
-
 public class User {
-    private String name;
-    private String role;
+    /// Name of the User
+    private final String name;
+
+    /// Role of the User
+    private final String role;
 
     public User(String name, String role) {
         this.name = name;
@@ -22,16 +22,21 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getRole() {
         return role;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getName(), user.getName()) && Objects.equals(getRole(), user.getRole());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getRole());
     }
 
     @Override
