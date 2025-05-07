b1d9287a1e24d27064dc59d4999938deb16604fd
Dan <wanstdaniel@gmail.com>
2025-05-07 00:27:57 -0400

    InventoryController work

--- src/controller/InventoryController.java
+++ src/controller/InventoryController.java
@@ -4,7 +4,47 @@ package controller;
 //Daniel and Anna
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.hsqldb.Table;
+
+
 public class InventoryController {
+
+    @FXML
+    public TextField searchField;
+    @FXML
+    public Button searchButton;
+    @FXML
+    public Button CASbutton;
+    @FXML
+    public Button adminBtn;
+    @FXML
+    public Button logoutButton;
+    @FXML
+    public TableView inventoryTable;
+    @FXML
+    public TableColumn chemicalColumn;
+    @FXML
+    public TableColumn concentrationColumn;
+    @FXML
+    public TableColumn companyColumn;
+    @FXML
+    public TableColumn shelfColumn;
+    @FXML
+    public TableColumn jarsColumn;
+    @FXML
+    public TableColumn amountColumn;
+    @FXML
+    public TableColumn unitColumn;
+    @FXML
+    public TableColumn dropperColumn;
+    @FXML
+    public TableColumn CAScolumn;
+
 
 
 
