0579f845f973153a5574a619d5a2d60788e7aa98
Jordan Andersen <bokeh.yap.0l@icloud.com>
2025-05-08 13:20:57 -0400

    Added Main.java for Mac IntelliJ IDEA users.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * The main method that launches the JavaFX application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        MainApp.launch(MainApp.class, args);
+    }
+}
