2d2b7245dbbd525ec9e592d4f55723ae4a8d195a
Jordan Andersen <bokeh.yap.0l@icloud.com>
2025-05-08 11:04:52 -0400

    Removed unnecessary Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    /**
-     * The main method that launches the JavaFX application.
-     *
-     * @param args command line arguments
-     */
-    public static void main(String[] args) { MainApp.launch(MainApp.class, args);
-    }
-}
