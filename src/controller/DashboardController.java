5cf8a85e8a571f2af05028f88b4f39bdc6d1c0a8
Christina Kc <110747227+chrissyckc@users.noreply.github.com>
2025-05-15 20:35:39 -0400

    Update DashboardController.java

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -1,10 +1,7 @@
-//CHRISTINA KC
-
 package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.fxml.FXMLLoader;
@@ -14,119 +11,83 @@ import model.User;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Enumeration;
 
 public class DashboardController {
 
     @FXML private ToggleButton searchBtn, addBtn, locationBtn, sdsBtn, orderBtn, adminBtn;
-
-    private ToggleGroup sidebarGroup;
     private User loggedInUser;
-
-    public void setLoggedInUser(User user) {
-        this.loggedInUser = user;
-        System.out.println("Logged in user: " + loggedInUser.getName());
-    }
 
     @FXML
     public void initialize() {
-        listResourcesDebug();
-
-        System.out.println("searchBtn = " + searchBtn);
-        System.out.println("addBtn = " + addBtn);
-        System.out.println("locationBtn = " + locationBtn);
-        System.out.println("sdsBtn = " + sdsBtn);
-        System.out.println("orderBtn = " + orderBtn);
-        System.out.println("adminBtn = " + adminBtn);
-
-        sidebarGroup = new ToggleGroup();
-
+        System.out.println("DashboardController initialized");
+        verifyResources();
+        
+        ToggleGroup sidebarGroup = new ToggleGroup();
         searchBtn.setToggleGroup(sidebarGroup);
+        adminBtn.setToggleGroup(sidebarGroup);
         addBtn.setToggleGroup(sidebarGroup);
         locationBtn.setToggleGroup(sidebarGroup);
         sdsBtn.setToggleGroup(sidebarGroup);
         orderBtn.setToggleGroup(sidebarGroup);
-        adminBtn.setToggleGroup(sidebarGroup);
 
-        searchBtn.setSelected(true);  // default selection is inventory
+        searchBtn.setSelected(true);
+        loadCenterModule("/inventory.fxml");
+    }
 
-        Platform.runLater(() -> {
-            loadCenterModule("inventory.fxml");  //
-        });
+    private void verifyResources() {
+        System.out.println("Verifying resources:");
+        System.out.println("Inventory icon: " + getClass().getResource("/images/icons/inventory.png"));
+        System.out.println("Inventory FXML: " + getClass().getResource("/inventory.fxml"));
+    }
+
+    public void setLoggedInUser(User user) {
+        this.loggedInUser = user;
+    }
+
+    private void loadCenterModule(String fxmlPath) {
+        try {
+            System.out.println("Loading: " + fxmlPath);
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Node content = loader.load();
+            
+            if (loader.getController() instanceof InventoryController) {
+                ((InventoryController)loader.getController()).setCurrentUser(loggedInUser);
+            }
+            
+            ((BorderPane) searchBtn.getScene().getRoot()).setCenter(content);
+        } catch (IOException e) {
+            System.err.println("Failed to load: " + fxmlPath);
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void handleSearch() {
-        searchBtn.setSelected(true);
-        loadCenterModule("inventory.fxml");  //
-    }
-
-    @FXML
-    private void handleAdd() {
-        addBtn.setSelected(true);
-        loadCenterModule("add.fxml");
-    }
-
-    @FXML
-    private void handleLocation() {
-        locationBtn.setSelected(true);
-        loadCenterModule("location.fxml");
-    }
-
-    @FXML
-    private void handleSDS() {
-        sdsBtn.setSelected(true);
-        loadCenterModule("sds.fxml");
-    }
-
-    @FXML
-    private void handleOrder() {
-        orderBtn.setSelected(true);
-        loadCenterModule("order.fxml");
+        loadCenterModule("/inventory.fxml");
     }
 
     @FXML
     private void handleAdmin() {
-        adminBtn.setSelected(true);
-        loadCenterModule("admin.fxml");
+        loadCenterModule("/admin.fxml");
     }
 
-    private void loadCenterModule(String fxmlFileName) {
-        try {
-            String fullPath = "fxml/" + fxmlFileName;
-            URL resource = getClass().getClassLoader().getResource(fullPath);
-            if (resource == null) {
-                throw new IOException("Resource not found: " + fullPath);
-            }
-
-            FXMLLoader loader = new FXMLLoader(resource);
-            Node content = loader.load();
-
-            Parent root = searchBtn.getScene().getRoot();
-            if (root instanceof BorderPane mainPane) {
-                mainPane.setCenter(content);
-            } else {
-                System.err.println("Root is not BorderPane!");
-            }
-
-            System.out.println("Successfully loaded: " + fxmlFileName);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Failed to load module: " + fxmlFileName);
-        }
+    @FXML
+    private void handleAdd() {
+        loadCenterModule("/add.fxml");
     }
 
-    public void listResourcesDebug() {
-        System.out.println("=== Listing resources under fxml/modules/ ===");
-        try {
-            Enumeration<URL> resources = getClass().getClassLoader().getResources("fxml/modules");
-            while (resources.hasMoreElements()) {
-                System.out.println("Found: " + resources.nextElement());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @FXML
+    private void handleLocation() {
+        loadCenterModule("/location.fxml");
+    }
+
+    @FXML
+    private void handleSDS() {
+        loadCenterModule("/sds.fxml");
+    }
+
+    @FXML
+    private void handleOrder() {
+        loadCenterModule("/order.fxml");
     }
 }
-
